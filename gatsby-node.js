23901e117917fa22b435633cba05e55020dfc507
rbrynteson <rbrynteson@brynteson.info>
2022-02-21 15:26:45 -0600

    Remove Random Blog Archive page

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,9 +27,6 @@ exports.createPages = async gatsbyUtilities => {
 
     // If there are posts, create pages for them
     await createIndividualBlogPostPages({ posts, gatsbyUtilities });
-
-    // And a paginated archive
-    await createBlogPostArchive({ posts, gatsbyUtilities });
 };
 
 /**
@@ -89,68 +86,6 @@ const createIndividualBlogPostPages = async ({ posts, gatsbyUtilities }) =>
             })
         )
     );
-
-/**
- * This function creates all the individual blog pages in this site
- */
-async function createBlogPostArchive({ posts, gatsbyUtilities }) {
-    const graphqlResult = await gatsbyUtilities.graphql(/* GraphQL */ `
-        {
-            wp {
-                readingSettings {
-                    postsPerPage
-                }
-            }
-        }
-    `);
-
-    const { postsPerPage } = graphqlResult.data.wp.readingSettings;
-
-    const postsChunkedIntoArchivePages = chunk(posts, postsPerPage);
-    const totalPages = postsChunkedIntoArchivePages.length;
-
-    return Promise.all(
-        postsChunkedIntoArchivePages.map(async (_posts, index) => {
-            const pageNumber = index + 1;
-
-            const getPagePath = page => {
-                if (page > 0 && page <= totalPages) {
-                    // Since our homepage is our blog page
-                    // we want the first page to be "/" and any additional pages
-                    // to be numbered.
-                    // "/blog/2" for example
-                    return page === 1 ? `/` : `/blog/${page}`;
-                }
-
-                return null;
-            };
-
-            // createPage is an action passed to createPages
-            // See https://www.gatsbyjs.com/docs/actions#createPage for more info
-            await gatsbyUtilities.actions.createPage({
-                path: getPagePath(pageNumber),
-
-                // use the blog post archive template as the page component
-                component: path.resolve(`./src/templates/blog-post-archive.js`),
-
-                // `context` is available in the template as a prop and
-                // as a variable in GraphQL.
-                context: {
-                    // the index of our loop is the offset of which posts we want to display
-                    // so for page 1, 0 * 10 = 0 offset, for page 2, 1 * 10 = 10 posts offset,
-                    // etc
-                    offset: index * postsPerPage,
-
-                    // We need to tell the template how many posts to display too
-                    postsPerPage,
-
-                    nextPagePath: getPagePath(pageNumber + 1),
-                    previousPagePath: getPagePath(pageNumber - 1),
-                },
-            });
-        })
-    );
-}
 
 /**
  * This function queries Gatsby's GraphQL server and asks for
